3f683cbc37ccdf0026b9051eb436f7a8d478c693
legoushka <96149010+legoushka@users.noreply.github.com>
2022-05-23 20:48:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cryptoapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import io.reactivex.disposables.CompositeDisposable
+
+private val compositeDisposable = CompositeDisposable()
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
+        viewModel.priceList.observe(this, Observer {
+            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it ")
+        })
+    }
+}
